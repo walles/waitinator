ff90f7585f9973d905f7334d680dcb47061cb94d
Johan Walles <johan.walles@gmail.com>
2022-06-04 10:32:31 +0200

    Lesson 2, part 5 (Interactivity) done

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
@@ -51,11 +52,28 @@ class _RandomWordsState extends State<RandomWords> {
             _suggestions.addAll(generateWordPairs().take(10)); /*4*/
           }
 
+          final alreadySaved = _saved.contains(_suggestions[index]);
+
           return ListTile(
-              title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
-          ));
+            title: Text(
+              _suggestions[index].asPascalCase,
+              style: _biggerFont,
+            ),
+            trailing: Icon(
+              alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.red : null,
+              semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+            ),
+            onTap: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(_suggestions[index]);
+                } else {
+                  _saved.add(_suggestions[index]);
+                }
+              });
+            },
+          );
         });
   }
 }
