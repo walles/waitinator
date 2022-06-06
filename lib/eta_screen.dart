32af1c6f222e0680958077cb19042fd37fb89619
Johan Walles <johan.walles@gmail.com>
2022-06-06 16:10:56 +0200

    Add initial observation to a list

--- lib/eta_screen.dart
+++ lib/eta_screen.dart
@@ -3,8 +3,11 @@ import 'package:waitinator/screen_wrapper.dart';
 
 class EtaScreen extends StatelessWidget {
   final int _target;
-  const EtaScreen(int target, int current, {Key? key})
+  final List<_Observation> _observations;
+
+  EtaScreen(int target, int current, {Key? key})
       : _target = target,
+        _observations = [_Observation(DateTime.now(), current)],
         super(key: key);
 
   @override
@@ -18,11 +21,21 @@ class EtaScreen extends StatelessWidget {
         textAlign: TextAlign.right,
       )),
       const Flexible(child: Text("[Line with a timestamp and its number]")),
-      const Flexible(child: Text("[...]")),
+      Flexible(
+          child: Text(
+              "... list of ${_observations.length.toString()} entries...")),
       const Flexible(child: Text("[Line with a timestamp and its number]")),
       const Flexible(
           child:
               Text("[Line with timestamp, number entry and an Enter button]")),
     ]);
   }
+}
+
+class _Observation {
+  final DateTime timestamp;
+  final int position;
+
+  // FIXME: Should we validate the input here?
+  _Observation(this.timestamp, this.position);
 }
