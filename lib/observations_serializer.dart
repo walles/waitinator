f8bd4c8df7d8d69a3ea016e34c405cda62ee12ba
Johan Walles <johan.walles@gmail.com>
2022-08-09 09:13:10 +0200

    WIP: Broken serialization but with tests
    Tests fail, implement properly.

---
+++ lib/observations_serializer.dart
@@ -0,0 +1,12 @@
+import 'package:meta/meta.dart';
+import 'package:waitinator/observation.dart';
+
+@visibleForTesting
+String serializeObservations(List<Observation> observations) {
+  return "";
+}
+
+@visibleForTesting
+List<Observation> deserializeObservations(String serialized) {
+  return [];
+}
