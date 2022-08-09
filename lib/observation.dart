11306ee117fac484f5f77d6206d84d6d643452ef
Johan Walles <johan.walles@gmail.com>
2022-08-09 15:19:41 +0200

    Implement serialization properly

--- lib/observation.dart
+++ lib/observation.dart
@@ -8,4 +8,26 @@ class Observation {
   String toString() {
     return "{ pos=$position time=$timestamp }";
   }
+
+  @override
+  bool operator ==(Object other) {
+    if (other.runtimeType != Observation) {
+      return false;
+    }
+
+    Observation otherObservation = other as Observation;
+    if (otherObservation.position != position) {
+      return false;
+    }
+    if (otherObservation.timestamp != timestamp) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @override
+  int get hashCode {
+    return position.hashCode ^ timestamp.hashCode;
+  }
 }
--- lib/observations_serializer.dart
+++ lib/observations_serializer.dart
@@ -3,10 +3,47 @@ import 'package:waitinator/observation.dart';
 
 @visibleForTesting
 String serializeObservations(List<Observation> observations) {
-  return "";
+  String serialized = "";
+
+  for (final observation in observations) {
+    if (serialized.isNotEmpty) {
+      serialized += " ";
+    }
+
+    serialized += observation.position.toString();
+    serialized += " ";
+    serialized += observation.timestamp.millisecondsSinceEpoch.toString();
+  }
+
+  return serialized;
 }
 
+/// This function will return an empty list on any parse errors.
 @visibleForTesting
 List<Observation> deserializeObservations(String serialized) {
-  return [];
+  final strings = serialized.split(" ");
+  if (strings.length % 2 != 0) {
+    // We need an even number of strings, since each entry consists of two
+    // strings.
+    return [];
+  }
+
+  List<int> numbers = [];
+  for (final string in strings) {
+    final number = int.parse(string);
+    if (number.isNaN) {
+      return [];
+    }
+
+    numbers.add(number);
+  }
+
+  List<Observation> result = [];
+  for (int i = 0; i < numbers.length; i += 2) {
+    final position = numbers[i];
+    final timestamp = DateTime.fromMillisecondsSinceEpoch(numbers[i + 1]);
+    result.add(Observation(timestamp, position));
+  }
+
+  return result;
 }
