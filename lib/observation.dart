a39c71bc68992c3011814c4e728fccab55db1673
Johan Walles <johan.walles@gmail.com>
2022-08-09 09:31:01 +0200

    Improve test output

--- lib/observation.dart
+++ lib/observation.dart
@@ -3,4 +3,9 @@ class Observation {
   final int position;
 
   Observation(this.timestamp, this.position);
+
+  @override
+  String toString() {
+    return "{ pos=$position time=$timestamp }";
+  }
 }
