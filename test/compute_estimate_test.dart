e42b1754208c26c51673dbf2314fbf5fd92ad980
Johan Walles <johan.walles@gmail.com>
2022-08-11 20:15:47 +0200

    Fix broken tests

--- test/compute_estimate_test.dart
+++ test/compute_estimate_test.dart
@@ -1,48 +1,47 @@
 import 'package:test/test.dart';
 import 'package:waitinator/compute_estimate.dart';
+import 'package:waitinator/eta_state.dart';
 import 'package:waitinator/observation.dart';
 
 void main() {
   test('_getLastObservation() happy case', () {
-    expect(
-        getLastObservation([
-          Observation(DateTime.now(), 1),
-          Observation(DateTime.now(), 2)
-        ])!
-            .position,
-        equals(2));
+    final state = EtaState(55);
+    state.add(Observation(DateTime.now(), 1));
+    state.add(Observation(DateTime.now(), 2));
+
+    expect(getLastObservation(state)!.position, equals(2));
   });
 
   test('_getLastObservation() on an empty list returns null', () {
-    expect(getLastObservation([]), isNull);
+    expect(getLastObservation(EtaState(55)), isNull);
   });
 
   test('_getLastObservation() on single entry list returns null', () {
-    expect(getLastObservation([Observation(DateTime.now(), 1)]), isNull);
+    final state = EtaState(55);
+    state.add(Observation(DateTime.now(), 1));
+
+    expect(getLastObservation(state), isNull);
   });
 
   test(
-      '_getLastObservation() with multiple same-numbered final entries returns the first one of those',
+      '_getLastObservation() with multiple same-numbered final entries returns the earliest one of those',
       () {
     var correctMarker = DateTime.fromMillisecondsSinceEpoch(1234);
-    expect(
-        getLastObservation([
-          Observation(DateTime.now(), 1),
-          Observation(correctMarker, 2),
-          Observation(DateTime.now(), 2),
-        ])!
-            .timestamp,
-        equals(correctMarker));
+    final state = EtaState(55);
+    state.add(Observation(DateTime.now(), 1));
+    state.add(Observation(correctMarker, 2));
+    state.add(Observation(DateTime.now(), 2));
+
+    expect(getLastObservation(state)!.timestamp, equals(correctMarker));
   });
 
   test(
       '_getLastObservation() with all entries containing the same number returns null',
       () {
-    expect(
-        getLastObservation([
-          Observation(DateTime.now(), 1),
-          Observation(DateTime.now(), 1),
-        ]),
-        isNull);
+    final state = EtaState(55);
+    state.add(Observation(DateTime.now(), 1));
+    state.add(Observation(DateTime.now(), 1));
+
+    expect(getLastObservation(state), isNull);
   });
 }
